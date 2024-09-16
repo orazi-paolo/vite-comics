8bc940c45eef25e122b69b64329e4d45ea5c81b6
orazi-paolo <orazi.paolo10@gmail.com>
2024-09-16 17:46:12 +0200

    completed footer

--- src/components/SignUpNavbar.vue
+++ src/components/SignUpNavbar.vue
@@ -9,8 +9,51 @@ export default {
 </script>
 
 <template>
+  <div class="navbar-footer">
+    <div class="container">
+      <button type="button">SIGN-UP NOW!</button>
+      <div class="nav-footer">
+        <p>FOLLOW US</p>
+        <a href="#"><img src="../assets/img/footer-facebook.png" alt="Facebook"></a>
+        <a href="#"><img src="../assets/img/footer-twitter.png" alt="Twitter"></a>
+        <a href="#"><img src="../assets/img/footer-youtube.png" alt="Youtube"></a>
+        <a href="#"><img src="../assets/img/footer-pinterest.png" alt="Pinterest"></a>
+        <a href="#"><img src="../assets/img/footer-periscope.png" alt="Periscope"></a>
+      </div>
+
+    </div>
+  </div>
   
 </template>
 
 <style scoped>
+.navbar-footer{
+  background-color: #303030;
+  padding: 40px 0 40px 0;
+    .container{
+      display: flex;
+      justify-content: space-between;
+      button{
+        border-color: #0082f9;
+        background-color: #303030;
+        padding: 10px;
+        color: white;
+        cursor: pointer;
+      }
+      .nav-footer{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 30%;
+        p{
+          color: #0082f9;
+          font-weight: bold;
+        }
+        img{
+          height: 30px;
+        }
+      }
+    }
+
+}
 </style>
