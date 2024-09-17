181d5f03fefd84db5e41bd71aa878072e3905121
orazi-paolo <orazi.paolo10@gmail.com>
2024-09-17 16:23:22 +0200

    added thumbs dynamically

---
+++ src/components/AppMainCurrentSeries.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  props: {
+    thumbImageUrl: {
+      type: String,
+      required: true
+    },
+    thumbPrice: {
+      type: String,
+      required: false
+    },
+    thumbSeries: {
+      type: String,
+      required: true
+    },
+    thumbType: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
+
+<template>
+        <div class="thumb">
+            <img :src="thumbImageUrl" :alt="thumbSeries" />
+            <p>{{ thumbSeries }}</p>
+        </div>
+</template>
+
+<style lang="scss" scoped>
+    .thumb{
+        height: 150px;
+        width: calc((100% / 6) - 20px);
+        text-align: center;
+        img{
+            height: 100px;
+            width: 100%;
+            object-fit: cover;
+            object-position: top;
+       }
+        p{
+            color: white;
+            text-align: center;
+        }
+    }
+</style>
